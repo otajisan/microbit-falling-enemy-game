1fe6536bb97d78c93d4b11e59e949dda4ca18b4a
Taji <taji.taji.otajisan@gmail.com>
2021-03-30 12:52:39 +0900

    refactoring

--- main.py
+++ main.py
@@ -1,55 +1,53 @@
-def init_enemy(enemy: game.LedSprite):
+def init_enemy():
     enemy.set(LedSpriteProperty.Y, 0)
     enemy.set(LedSpriteProperty.X, randint(0, 4))
     return enemy
+
 def accelerate_enemy(game_round: number):
-    sleep_time2 = 1000 / game_round ^ 1 / 4
-    if sleep_time2 < 150:
-        sleep_time2 = 150
-    return sleep_time2
+    sleep_time = 1000 / game_round ^ 1 / 4
+    if sleep_time < 150:
+        sleep_time = 150
+    return sleep_time
 
 def on_button_pressed_a():
     hero.change(LedSpriteProperty.X, -1)
     music.play_tone(523, music.beat(BeatFraction.EIGHTH))
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def initialize():
-    global sleep_time, game_round, enemy
-    music.start_melody(music.built_in_melody(Melodies.POWER_UP), MelodyOptions.ONCE)
-    game.set_life(3)
-    sleep_time = 1000
-    game_round = 1
-    enemy = game.create_sprite(0, 0)
-    enemy = init_enemy(enemy)
-
 def on_button_pressed_b():
     hero.change(LedSpriteProperty.X, 1)
     music.play_tone(523, music.beat(BeatFraction.EIGHTH))
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-def fall_enemy():
-    global sleep_time, enemy, game_round
-    while game.is_game_over() == False:
-        sleep_time = accelerate_enemy(game_round)
-        basic.pause(sleep_time)
-        enemy.set(LedSpriteProperty.Y, enemy.get(LedSpriteProperty.Y) + 1)
-        if enemy.get(LedSpriteProperty.Y) == 4:
-            if enemy.get(LedSpriteProperty.X) == hero.get(LedSpriteProperty.X):
-                game.remove_life(1)
-                music.play_tone(196, music.beat(BeatFraction.EIGHTH))
-            else:
-                game.add_score(1)
-                music.play_tone(784, music.beat(BeatFraction.EIGHTH))
-            basic.pause(sleep_time)
-            enemy = init_enemy(enemy)
-            game_round = game_round + 1
-enemy: game.LedSprite = None
-sleep_time = 0
-hero: game.LedSprite = None
-game_round = 0
-hero = game.create_sprite(2, 4)
-initialize()
+def initialize():
+    music.start_melody(music.built_in_melody(Melodies.POWER_UP), MelodyOptions.ONCE)
+    game.set_life(3)
+    enemy = init_enemy()
+
+def fall_enemy(sleep_time):
+    global enemy, game_round
+    sleep_time = accelerate_enemy(game_round)
+    basic.pause(sleep_time)
+    enemy.set(LedSpriteProperty.Y, enemy.get(LedSpriteProperty.Y) + 1)
+    if enemy.get(LedSpriteProperty.Y) == 4:
+        if enemy.get(LedSpriteProperty.X) == hero.get(LedSpriteProperty.X):
+            game.remove_life(1)
+            music.play_tone(196, music.beat(BeatFraction.EIGHTH))
+        else:
+            game.add_score(1)
+            music.play_tone(784, music.beat(BeatFraction.EIGHTH))
+        enemy = init_enemy()
+        game_round = game_round + 1
+        
+
+hero: game.LedSprite = game.create_sprite(2, 4)
+enemy: game.LedSprite = game.create_sprite(0, 0)
+game_round = 1
 
 def on_forever():
-    fall_enemy()
+    initialize()
+    sleep_time = 1000
+    while True:
+        fall_enemy(sleep_time)
+
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,56 +1,59 @@
-function init_enemy (enemy: game.LedSprite) {
+function init_enemy(): game.LedSprite {
     enemy.set(LedSpriteProperty.Y, 0)
     enemy.set(LedSpriteProperty.X, randint(0, 4))
     return enemy
 }
-function accelerate_enemy (game_round: number) {
-    let sleep_time2 = 1000 / game_round ^ 1 / 4
-if (sleep_time2 < 150) {
-        sleep_time2 = 150
+
+function accelerate_enemy(game_round: number): number {
+    let sleep_time = 1000 / game_round ^ 1 / 4
+    if (sleep_time < 150) {
+        sleep_time = 150
     }
-    return sleep_time2
+    
+    return sleep_time
 }
-input.onButtonPressed(Button.A, function () {
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     hero.change(LedSpriteProperty.X, -1)
     music.playTone(523, music.beat(BeatFraction.Eighth))
 })
-function initialize () {
-    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
-    game.setLife(3)
-    sleep_time = 1000
-    game_round = 1
-    enemy = game.createSprite(0, 0)
-    enemy = init_enemy(enemy)
-}
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     hero.change(LedSpriteProperty.X, 1)
     music.playTone(523, music.beat(BeatFraction.Eighth))
 })
-function fall_enemy () {
-    while (game.isGameOver() == false) {
-        sleep_time = accelerate_enemy(game_round)
-        basic.pause(sleep_time)
-        enemy.set(LedSpriteProperty.Y, enemy.get(LedSpriteProperty.Y) + 1)
-        if (enemy.get(LedSpriteProperty.Y) == 4) {
-            if (enemy.get(LedSpriteProperty.X) == hero.get(LedSpriteProperty.X)) {
-                game.removeLife(1)
-                music.playTone(196, music.beat(BeatFraction.Eighth))
-            } else {
-                game.addScore(1)
-                music.playTone(784, music.beat(BeatFraction.Eighth))
-            }
-            basic.pause(sleep_time)
-            enemy = init_enemy(enemy)
-            game_round = game_round + 1
-        }
-    }
+function initialize() {
+    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+    game.setLife(3)
+    let enemy = init_enemy()
 }
-let enemy: game.LedSprite = null
-let sleep_time = 0
-let hero: game.LedSprite = null
-let game_round = 0
-hero = game.createSprite(2, 4)
-initialize()
-basic.forever(function () {
-    fall_enemy()
+
+function fall_enemy(sleep_time: number) {
+    
+    sleep_time = accelerate_enemy(game_round)
+    basic.pause(sleep_time)
+    enemy.set(LedSpriteProperty.Y, enemy.get(LedSpriteProperty.Y) + 1)
+    if (enemy.get(LedSpriteProperty.Y) == 4) {
+        if (enemy.get(LedSpriteProperty.X) == hero.get(LedSpriteProperty.X)) {
+            game.removeLife(1)
+            music.playTone(196, music.beat(BeatFraction.Eighth))
+        } else {
+            game.addScore(1)
+            music.playTone(784, music.beat(BeatFraction.Eighth))
+        }
+        
+        enemy = init_enemy()
+        game_round = game_round + 1
+    }
+    
+}
+
+let hero = game.createSprite(2, 4)
+let enemy = game.createSprite(0, 0)
+let game_round = 1
+basic.forever(function on_forever() {
+    initialize()
+    let sleep_time = 1000
+    while (true) {
+        fall_enemy(sleep_time)
+    }
 })
